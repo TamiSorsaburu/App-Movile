a81a432116872ec4609e180e35c2d59d1d1d4f9c
TamiSorsaburu <tamysorsaburu@gmail.com>
2023-09-21 18:24:07 -0300

    Se agrega activity iniciar sesión terminada

--- app/src/main/java/com/example/servicedelautomotor/Iniciar_sesion.java
+++ app/src/main/java/com/example/servicedelautomotor/Iniciar_sesion.java
@@ -2,7 +2,9 @@ package com.example.servicedelautomotor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Iniciar_sesion extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Iniciar_sesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+    }
+
+    public void buttonLogin(View view) {
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
     }
 }
