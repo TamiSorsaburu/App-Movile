843c79784c9b89db304b1e5f53cab8f518d73ac2
TamiSorsaburu <tamysorsaburu@gmail.com>
2023-09-21 20:29:02 -0300

    Se agrega compilacion de intent en activity register

--- app/src/main/java/com/example/servicedelautomotor/Register.java
+++ app/src/main/java/com/example/servicedelautomotor/Register.java
@@ -2,7 +2,9 @@ package com.example.servicedelautomotor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void buttonRegister(View view) {
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
+    }
+
+    public void textLogin(View view) {
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
     }
 }
