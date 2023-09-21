07f3754e9793df671d3323db038ca768d6e981a2
TamiSorsaburu <tamysorsaburu@gmail.com>
2023-09-21 10:41:24 -0300

    Se agrega activity presentacion y dashboard al repositorio

--- app/src/main/java/com/example/servicedelautomotor/MainActivity.java
+++ app/src/main/java/com/example/servicedelautomotor/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.servicedelautomotor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_presentacion);
+    }
+
+    public void buttonLogin(View view) {
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
     }
 }
