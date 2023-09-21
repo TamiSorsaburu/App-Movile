393bf45a0dadb9f611051b935d63584cbc51e4e1
TamiSorsaburu <tamysorsaburu@gmail.com>
2023-09-21 13:24:39 -0300

    Se crea activity iniciar sesión

---
+++ app/src/main/java/com/example/servicedelautomotor/Iniciar_sesion.java
@@ -0,0 +1,14 @@
+package com.example.servicedelautomotor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Iniciar_sesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sesion);
+    }
+}
