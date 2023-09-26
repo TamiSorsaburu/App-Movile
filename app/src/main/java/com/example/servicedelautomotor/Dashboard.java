93d1162d7a015501b1b71ea4828838722a498824
Margarita-Bravo <bravo_m33@hotmail.com>
2023-09-26 20:21:00 -0300

    form mecanico finalizado

--- app/src/main/java/com/example/servicedelautomotor/Dashboard.java
+++ app/src/main/java/com/example/servicedelautomotor/Dashboard.java
@@ -19,5 +19,39 @@ public class Dashboard extends AppCompatActivity {
         startActivity(mecanico);
     }
 
+    public void botonSiniestro(View V){
+        Intent siniestro=new Intent(this, SiniestrosActivity.class);
+        startActivity(siniestro);
+    }
+
+    /*
+    public void botonServicio(View V){
+        Intent servicio=new Intent(this, Servicio.class);
+        startActivity(servicio);
+    }*/
+
+     /*
+    public void botonGrua(View V){
+        Intent grua=new Intent(this, Grua.class);
+        startActivity(grua);
+    }*/
+
+     /*
+    public void botonMiAuto(View V){
+        Intent auto=new Intent(this, MiAuto.class);
+        startActivity(auto);
+    }*/
+
+     /*
+    public void botonContacto(View V){
+        Intent contacto=new Intent(this, Contacto.class);
+        startActivity(contacto);
+    }*/
+
+     /*
+    public void botonConfiguracion(View V){
+        Intent configuracion=new Intent(this, SiniestrosActivity.class);
+        startActivity(configuracion);
+    }*/
 
 }
