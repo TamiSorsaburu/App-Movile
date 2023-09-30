3c4c823d8300f081ff5fb874aa94c29099d2654f
Margarita-Bravo <bravo_m33@hotmail.com>
2023-09-30 14:23:19 -0300

    Corrigiendo navegabilidad

--- app/src/main/java/com/example/servicedelautomotor/MisTurnos.java
+++ app/src/main/java/com/example/servicedelautomotor/MisTurnos.java
@@ -1,6 +1,8 @@
 package com.example.servicedelautomotor;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,10 @@ public class MisTurnos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_turnos);
+    }
+
+    public void botonVolver(View V){
+        Intent volver=new Intent(this, Dashboard.class);
+        startActivity(volver);
     }
 }
--- app/src/main/java/com/example/servicedelautomotor/Servicios.java
+++ app/src/main/java/com/example/servicedelautomotor/Servicios.java
@@ -1,4 +1,13 @@
 package com.example.servicedelautomotor;
 
-public class Servicios {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Servicios extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servicios);
+    }
 }
