182b9c852286c4cffad7a2b8c721e41ab3397c94
RominaNavarrete <rominagnavarrete@gmail.com>
2023-09-30 17:43:56 -0300

    agregando navegabilidad a mis activitys

--- app/src/main/java/com/example/servicedelautomotor/SolicitarTurno.java
+++ app/src/main/java/com/example/servicedelautomotor/SolicitarTurno.java
@@ -2,7 +2,9 @@ package com.example.servicedelautomotor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SolicitarTurno extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class SolicitarTurno extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solicitar_turno);
+    }
+    public void botonConfirmar(View V) {
+        Intent confirmar = new Intent(this, ConfirmarTurno.class);
+        startActivity(confirmar);
+    }
+    public void botonCancelar(View V) {
+        Intent cancelar = new Intent(this, Presupuesto.class);
+        startActivity(cancelar);
     }
 }
