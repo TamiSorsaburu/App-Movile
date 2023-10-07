d701448c32de6858551f056480464caf8f8f5f2d
Margarita-Bravo <bravo_m33@hotmail.com>
2023-10-07 14:10:02 -0300

    cargando dependecias para BD

---
+++ app/src/main/java/com/example/servicedelautomotor/coneccionBD/AppDataBase.java
@@ -0,0 +1,10 @@
+package com.example.servicedelautomotor.coneccionBD;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {},version =1,exportSchema = true )
+public abstract class AppDataBase extends RoomDatabase {
+
+
+}
