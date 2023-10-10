5ef282bed79d75483197a709c518e76c3cab5520
TamiSorsaburu <tamysorsaburu@gmail.com>
2023-10-10 15:36:07 -0300

    Se crea  activity recuperar contraseña

---
+++ app/src/main/java/com/example/servicedelautomotor/RecuperarContrasena.java
@@ -0,0 +1,13 @@
+package com.example.servicedelautomotor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecuperarContrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_contrasena);
+    }
+}
