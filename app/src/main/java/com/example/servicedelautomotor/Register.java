920c0932fc0fbd4982521612e426f64d96e2c2c1
TamiSorsaburu <tamysorsaburu@gmail.com>
2023-10-19 13:17:32 -0300

    Se modifica Register.java, implementando el registro de usuarios a la base de datos

--- app/src/main/java/com/example/servicedelautomotor/Register.java
+++ app/src/main/java/com/example/servicedelautomotor/Register.java
@@ -1,26 +1,84 @@
 package com.example.servicedelautomotor;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import androidx.room.Room;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.servicedelautomotor.coneccionBD.AppDataBase;
+import com.example.servicedelautomotor.entidades.Usuario;
 
 public class Register extends AppCompatActivity {
+    private EditText usernameEditText;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText confirmPasswordEditText;
+    private Button registerButton;
 
+    private AppDataBase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        database = Room.databaseBuilder(getApplicationContext(), AppDataBase.class, "dbServiceAutomotor").build();
+
+        usernameEditText = findViewById(R.id.textUser);
+        emailEditText = findViewById(R.id.textCorreo);
+        passwordEditText = findViewById(R.id.textContraseña);
+        confirmPasswordEditText = findViewById(R.id.textConfirContraseña);
+        registerButton = findViewById(R.id.buttonRegistrar);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
     }
 
-    public void buttonRegister(View view) {
-        Intent intent = new Intent(this, Dashboard.class);
-        startActivity(intent);
+    private void registerUser() {
+        // Obtén los datos de los campos de entrada
+        String nombreUsuario = usernameEditText.getText().toString();
+        String correo = emailEditText.getText().toString();
+        String contraseña = passwordEditText.getText().toString();
+        String confirmContraseña = confirmPasswordEditText.getText().toString();
+
+        // Realiza validaciones de entrada y lógica de registro
+
+        if (contraseña.equals(confirmContraseña)) {
+            // Crea un nuevo objeto User
+            Usuario usuario = new Usuario("nombreUsuario", "correo", "contraseña");
+            usuario.setNombreUsuario(nombreUsuario);
+            usuario.setCorreo(correo);
+            usuario.setContraseña(contraseña);
+
+            // Inserta el usuario en la base de datos
+            new InsertUserTask().execute(usuario);
+
+            // Limpia los campos de entrada
+            usernameEditText.setText("");
+            emailEditText.setText("");
+            passwordEditText.setText("");
+            confirmPasswordEditText.setText("");
+
+            // Muestra un mensaje de registro exitoso
+            Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+        } else {
+            // Muestra un mensaje de error si las contraseñas no coinciden
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    public void textLogin(View view) {
-        Intent intent = new Intent(this, Iniciar_sesion.class);
-        startActivity(intent);
+    private class InsertUserTask extends AsyncTask<Usuario, Void, Void> {
+        @Override
+        protected Void doInBackground(Usuario... usuario) {
+            database.daoUsuario().insertarUsuario(usuario[0]);
+            return null;
+        }
     }
 }
