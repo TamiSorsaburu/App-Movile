f358e02d59d6d1eb5c9ddf9fce560d66c61fbc0f
EvanaSabatella1989 <eva.sabatella@gmail.com>
2023-10-23 18:18:54 -0300

    Agregar Servicio

--- app/src/main/java/com/example/servicedelautomotor/entidades/Servicio.java
+++ app/src/main/java/com/example/servicedelautomotor/entidades/Servicio.java
@@ -1,5 +1,6 @@
 package com.example.servicedelautomotor.entidades;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
@@ -10,4 +11,54 @@ public class Servicio {
     public String nombre;
     public String descripcion;
     public double precio;
+
+    //@ColumnInfo(name = "image", typeAffinity = ColumnInfo.BLOB)
+    private String image;
+
+    public Servicio(String nombre, String descripcion, double precio, String image) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.image = image;
+    }
+
+    public long getIdServicio() {
+        return idServicio;
+    }
+
+    public void setIdServicio(long idServicio) {
+        this.idServicio = idServicio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
