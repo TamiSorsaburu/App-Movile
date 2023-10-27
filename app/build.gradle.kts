ad69c8a63f8d0a15f85b61a06bc6081efd7b985d
TamiSorsaburu <tamysorsaburu@gmail.com>
2023-10-27 01:50:37 -0300

    Se agrega lógica para mostrar mapa y ubicacion en activity siniestros

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.2.0")
     implementation("com.google.firebase:firebase-analytics")
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/servicedelautomotor/SiniestrosActivity.java
+++ app/src/main/java/com/example/servicedelautomotor/SiniestrosActivity.java
@@ -1,5 +1,6 @@
 package com.example.servicedelautomotor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,9 +8,22 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
-public class SiniestrosActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class SiniestrosActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+    TextView txtLatitud, txtLongitd;
+    GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +43,27 @@ public class SiniestrosActivity extends AppCompatActivity {
             throw new NullPointerException("Something went wrong");
         }
 
+        txtLatitud = findViewById(R.id.TextUbicacion);
+        txtLongitd = findViewById(R.id.TextUbicacion);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.barraMenu);
+        Menu menu = bottomNavigationView.getMenu();
+        menu.findItem(R.id.menu_exit).setVisible(false); // Oculta el ícono de cierre de sesión
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                if (item.getItemId() == R.id.menu_home) {
+                    Intent intent = new Intent(SiniestrosActivity.this, Dashboard.class);
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void btnHome(View V) {
@@ -41,5 +76,38 @@ public class SiniestrosActivity extends AppCompatActivity {
         Intent confirma = new Intent(this, activity_confirma_ayuda.class);
         startActivity(confirma);
 
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        this.mMap.setOnMapClickListener(this);
+        this.mMap.setOnMapLongClickListener(this);
+
+        LatLng argentina = new LatLng(-37.1162298,-62.6648238);
+        mMap.addMarker(new MarkerOptions().position(argentina).title("Argentina"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(argentina));
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+        txtLatitud.setText("" + latLng.latitude);
+        txtLongitd.setText("" + latLng.longitude);
+
+        mMap.clear();
+        LatLng argentina = new LatLng(latLng.latitude,latLng.longitude);
+        mMap.addMarker(new MarkerOptions().position(argentina));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(argentina));
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        txtLatitud.setText("" + latLng.latitude);
+        txtLongitd.setText("" + latLng.longitude);
+
+        mMap.clear();
+        LatLng argentina = new LatLng(latLng.latitude,latLng.longitude);
+        mMap.addMarker(new MarkerOptions().position(argentina));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(argentina));
     }
 }
