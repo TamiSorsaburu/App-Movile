ea8fbd75e0d0b5a6056ca3a9e86cd5efb51f2b0d
TamiSorsaburu <tamysorsaburu@gmail.com>
2023-10-27 02:17:26 -0300

    Se agrega modificaciones en activity presupuesto

--- app/src/main/java/com/example/servicedelautomotor/PresupuestoActivity.java
+++ app/src/main/java/com/example/servicedelautomotor/PresupuestoActivity.java
@@ -6,10 +6,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PresupuestoActivity extends AppCompatActivity {
     private Switch switchLavadero, switchCambioAceite, switchTapizado, switchNeumaticos;
@@ -33,6 +37,22 @@ public class PresupuestoActivity extends AppCompatActivity {
         switchCambioAceite.setOnCheckedChangeListener(new SwitchChangeListener());
         switchTapizado.setOnCheckedChangeListener(new SwitchChangeListener());
         switchNeumaticos.setOnCheckedChangeListener(new SwitchChangeListener());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.barraMenu);
+        Menu menu = bottomNavigationView.getMenu();
+        menu.findItem(R.id.menu_exit).setVisible(false); // Oculta el ícono de cierre de sesión
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                if (item.getItemId() == R.id.menu_home) {
+                    Intent intent = new Intent(PresupuestoActivity.this, Dashboard.class);
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
 
     }
     public void botonConfirmar(View V) {
